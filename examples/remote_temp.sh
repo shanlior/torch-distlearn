caff1ad892202672ad03bf3e27c1e33ff339835b
shanlior <shanlior@gmail.omc>
2016-04-21 12:31:11 +0300

    updated running script

--- examples/remote_temp.sh
+++ examples/remote_temp.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # run 4 nodes
+numNodes=4
 
-# kill gpu lua
-# kill GPU PID $(nvidia-smi -g 0 | awk '$2=="Processes:" {p=1} p && $3 > 0 && $5~/luajit/ {print $3}')
-
+#################################################
+# Try to close the ports if they are already used
 
 if [ -z "$1" ]
   then
@@ -13,7 +13,6 @@ else
   port=$1
 fi
 
-numNodes=2
 
 currPort=$port
 for i in `seq 0 $numNodes`;
@@ -23,19 +22,14 @@ do
   currPort=$(($currPort + 1))
 done
 
-# OMP_NUM_THREADS=4 th remote_temp_server.lua --server 1 --numNodes 1 --nodeIndex 0 --batchSize 128 --port $port --verbose 0 &
-# th remote_temp.lua --numNodes 1 --nodeIndex 1 --batchSize 128 --cuda --gpu 1 --port $port --verbose 0 &
-
-# th remote_temp_server.lua  --cuda --gpu 1 --server 1 --numNodes 2 --nodeIndex 0 --batchSize 128 --port $port --save testNet &
-# th remote_temp.lua --cuda --gpu 1 --numNodes 2 --nodeIndex 1 --batchSize 128 --port $port &
-th remote_temp.lua --cuda --gpu 1 --numNodes 4 --nodeIndex 4 --batchSize 128 --port $port --host 10.185.36.35 &
+# OPTIONAL: Uncomment if you want to close all luajit Processes running on GPU's
+# kill $(nvidia-smi -g 0 | awk '$2=="Processes:" {p=1} p && $3 > 0 && $5~/luajit/ {print $3}')
 
 
+#################################################
 
-# th remote.lua --numNodes 3 --nodeIndex 2 --batchSize 256 --cuda --gpu 2 &
-# th client_remote.lua --cuda --gpu 1 --numNodes 3 --base 3 --nodeIndex 3 --batchSize 384 --host 10.185.36.35 --port 8040 &
-# th remote.lua --numNodes 4 --nodeIndex 3 --batchSize 512 --base 4 --host 10.185.36.45 port 15200 &
-# th remote.lua --numNodes 4 --nodeIndex 4 --batchSize 512 --base 4 --host 10.185.36.45 port 15200 &
+th remote_temp.lua --cuda --gpu 1 --numNodes $numNodes --nodeIndex 4 --batchSize 128 --port $port --host 10.185.36.35 &
+
 
 # wait for them all
 wait
